8da939b5522d2ef3600e3358cea89bcdc896a331
Neil Thistlethwaite <neilt1111@yahoo.com>
2017-07-27 10:26:41 -0400

    Update command line args

--- src/main/java/bdv/bigcat/util/HDFConverter.java
+++ src/main/java/bdv/bigcat/util/HDFConverter.java
@@ -48,7 +48,7 @@ public class HDFConverter {
 
 	static public class Parameters {
 		
-		@Parameter( names = { "--inputfile", "--input", "-i" }, description = "Input hdf5 file")
+		@Parameter( names = { "--inputfile", "--input", "-i" }, description = "Input HDF5 file")
 		public String inputHDF5 = null;
 		
 		@Parameter( names = { "--groupname", "--group", "-g" }, description = "Output group name (N5 group)")
@@ -59,18 +59,25 @@ public class HDFConverter {
 		
 		@Parameter( names = { "--cellsize", "-c"}, description = "Size of cells to use in the output N5 dataset" )
 		public List<Integer> cellSize = Arrays.asList(new Integer[] {64, 64, 8});
+
+		@Parameter( names = { "--label", "-l" }, description = "Path to labels in HDF5 file" )
+		public String labelpath = "/volumes/labels/neuron_ids";		
 		
 		
 		public boolean init() {
 			if(outputGroupName == null || inputHDF5 == null)
 				return false;
+			
+			if(outputDatasetName == null)
+				outputDatasetName = (inputHDF5.contains(".hdf")?inputHDF5.substring(0, inputHDF5.lastIndexOf(".hdf")):inputHDF5);
+			
 			return true;
 		}
 	}
 	
-	private static int[] cellDimensions = new int[] {64, 64, 8};
-	private static String groupName = "/home/thistlethwaiten/n5-test/";
-	private static String datasetName = "sample_A_n5";
+//	private static int[] cellDimensions = new int[] {64, 64, 8};
+//	private static String outputGroupName = "/home/thistlethwaiten/n5-test/";
+//	private static String outputDatasetName = "sample_A_n5";
 
 	public static void main(String[] args) throws IOException {
 		final Parameters params = new Parameters();
@@ -80,6 +87,10 @@ public class HDFConverter {
 			jcomm.usage();
 			return;
 		}
+		
+		convertHDF5toN5("/home/thistlethwaiten/Downloads/sample_A_20160501.hdf", "/volumes/labels/neuron_ids",
+				new int[] {64, 64, 8}, "/home/thistlethwaiten/n5-test/", "sample_A_n5");
+		
 //		sampleConvert();
 //		Bdv bdv = sampleDisplay("sampleA", BdvOptions.options());
 //		sampleDisplay("sampleA-downscaled-4x4x1", BdvOptions.options().addTo(bdv));
@@ -89,9 +100,10 @@ public class HDFConverter {
 //		bdv.getBdvHandle().getViewerPanel().setDisplayMode(DisplayMode.SINGLE);
 	}
 	
-	public static Bdv sampleDisplay(String dname, BdvOptions options) throws IOException {
+	
+	public static Bdv sampleDisplay(String gname, String dname, BdvOptions options) throws IOException {
 
-		final N5Reader reader = N5.openFSReader(groupName);
+		final N5Reader reader = N5.openFSReader(gname);
 		
 		final DatasetAttributes attr = reader.getDatasetAttributes(dname);
 		
@@ -112,7 +124,8 @@ public class HDFConverter {
 		return BdvFunctions.show(volatileConverted, "BDV - " + dname, options);
 	}
 	
-	public static void sampleConvert() throws IOException {
+	
+	public static void convertHDF5toN5(String hdf5Path, String labelsPath, int[] cellDimensions, String outputGroupName, String outputDatasetName) throws IOException {
 		
 		IHDF5Reader reader = HDF5Factory.open("/home/thistlethwaiten/Downloads/sample_A_20160501.hdf");
 		
@@ -131,8 +144,8 @@ public class HDFConverter {
 		System.out.println(String.format("Dims are %s", Arrays.toString(dimensions)));
 
 		
-		N5Writer n5 = N5.openFSWriter(groupName);
-		n5.createDataset(datasetName, dimensions, cellDimensions, DataType.UINT8, CompressionType.RAW);
+		N5Writer n5 = N5.openFSWriter(outputGroupName);
+		n5.createDataset(outputDatasetName, dimensions, cellDimensions, DataType.UINT8, CompressionType.RAW);
 		
 		
 		final long[] offset = new long[nDim];
@@ -145,7 +158,7 @@ public class HDFConverter {
 			
 			IntervalView<LabelMultisetType> tm = Views.offsetInterval(img, offset, Arrays.stream(actualCellDimensions).asLongStream().toArray());
 						
-			final DatasetAttributes attributes = n5.getDatasetAttributes(datasetName);
+			final DatasetAttributes attributes = n5.getDatasetAttributes(outputDatasetName);
 			
 			long[] cellOffset = new long[nDim];
 			
@@ -156,7 +169,7 @@ public class HDFConverter {
 			final ByteArrayDataBlock dataBlock = new ByteArrayDataBlock(actualCellDimensions, cellOffset,
 					LabelUtils.serializeLabelMultisetTypes(Views.flatIterable(tm), (int)Intervals.numElements(actualCellDimensions)));
 			
-			n5.writeBlock(datasetName, attributes, dataBlock);
+			n5.writeBlock(outputDatasetName, attributes, dataBlock);
 	
 			
 			for( d = 0; d < nDim; d++) {
