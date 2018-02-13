a6dbe9308ed0dd150dfa39f276f44e229f6a7c2b
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2018-02-13 16:34:38 -0500

    Remove bigcat dependency
    Depending on a UI/application library is not desirable. It was necessary to extract LabelMultisetType and related classes into separate repositories first.

--- src/main/java/bdv/bigcat/spark/SparkDownsampleFunction.java
+++ src/main/java/bdv/bigcat/spark/SparkDownsampleFunction.java
@@ -10,13 +10,13 @@ import org.janelia.saalfeldlab.n5.N5FSWriter;
 import org.janelia.saalfeldlab.n5.N5Reader;
 import org.janelia.saalfeldlab.n5.N5Writer;
 
-import bdv.labels.labelset.Label;
-import bdv.labels.labelset.LabelMultisetType;
-import bdv.labels.labelset.LabelMultisetTypeDownscaler;
-import bdv.labels.labelset.LabelUtils;
-import bdv.labels.labelset.Multiset.Entry;
-import bdv.labels.labelset.N5CacheLoader;
-import bdv.labels.labelset.VolatileLabelMultisetArray;
+import net.imglib.type.label.Label;
+import net.imglib.type.label.LabelMultisetType;
+import net.imglib.type.label.LabelMultisetTypeDownscaler;
+import net.imglib.type.label.LabelUtils;
+import net.imglib.type.label.Multiset.Entry;
+import net.imglib.type.label.N5CacheLoader;
+import net.imglib.type.label.VolatileLabelMultisetArray;
 import net.imglib2.RandomAccessible;
 import net.imglib2.cache.CacheLoader;
 import net.imglib2.cache.img.CachedCellImg;
@@ -26,16 +26,23 @@ import net.imglib2.img.cell.Cell;
 import net.imglib2.img.cell.CellGrid;
 import net.imglib2.view.Views;
 
-public class SparkDownsampleFunction implements Function<DownsampleBlock, Integer> {
-	
+public class SparkDownsampleFunction implements Function< DownsampleBlock, Integer >
+{
+
 	private static final long serialVersionUID = 1384028449836651390L;
+
 	private final String inputGroupName;
+
 	private final String inputDatasetName;
+
 	private final int[] factor;
+
 	private final String outputGroupName;
+
 	private final String outputDatasetName;
 
-	public SparkDownsampleFunction(String inputGroupName, String inputDatasetName, int[] factor, String outputGroupName, String outputDatasetName ) {
+	public SparkDownsampleFunction( final String inputGroupName, final String inputDatasetName, final int[] factor, final String outputGroupName, final String outputDatasetName )
+	{
 		this.inputGroupName = inputGroupName;
 		this.inputDatasetName = inputDatasetName;
 		this.factor = factor;
@@ -44,77 +51,81 @@ public class SparkDownsampleFunction implements Function<DownsampleBlock, Intege
 	}
 
 	@Override
-	public Integer call(DownsampleBlock targetRegion) throws Exception {
+	public Integer call( final DownsampleBlock targetRegion ) throws Exception
+	{
 
-		final N5Reader reader = new N5FSReader(inputGroupName);
-		final DatasetAttributes attr = reader.getDatasetAttributes(inputDatasetName);
-		
+		final N5Reader reader = new N5FSReader( inputGroupName );
+		final DatasetAttributes attr = reader.getDatasetAttributes( inputDatasetName );
+
 		final long[] dimensions = attr.getDimensions();
 		final int[] blocksize = attr.getBlockSize();
-		
+
 		final int nDim = dimensions.length;
-		final long[] offset = new long[nDim];
-		
+		final long[] offset = new long[ nDim ];
+
 		final int[] targetSize = targetRegion.getSize();
 		final long[] targetMin = targetRegion.getMin();
-		
-		long[] actualLocation = new long[nDim];
-		long[] actualSize = new long[nDim];
-		
-		
-		final CacheLoader< Long, Cell< VolatileLabelMultisetArray > > cacheLoader = new N5CacheLoader(reader, inputDatasetName);
-		
+
+		final long[] actualLocation = new long[ nDim ];
+		final long[] actualSize = new long[ nDim ];
+
+		final CacheLoader< Long, Cell< VolatileLabelMultisetArray > > cacheLoader = new N5CacheLoader( reader, inputDatasetName );
+
 		final BoundedSoftRefLoaderCache< Long, Cell< VolatileLabelMultisetArray > > cache = new BoundedSoftRefLoaderCache<>( 1 );
 		final LoaderCacheAsCacheAdapter< Long, Cell< VolatileLabelMultisetArray > > wrappedCache = new LoaderCacheAsCacheAdapter<>( cache, cacheLoader );
-		
-		final CachedCellImg<LabelMultisetType,VolatileLabelMultisetArray> inputImg = new CachedCellImg<LabelMultisetType,VolatileLabelMultisetArray>(
-				new CellGrid(dimensions, blocksize), new LabelMultisetType(), wrappedCache, new VolatileLabelMultisetArray(0, true));
-		
+
+		final CachedCellImg< LabelMultisetType, VolatileLabelMultisetArray > inputImg = new CachedCellImg<>(
+				new CellGrid( dimensions, blocksize ), new LabelMultisetType(), wrappedCache, new VolatileLabelMultisetArray( 0, true ) );
+
 		int eachCount = 0;
-		for(Entry<Label> e : inputImg.firstElement().entrySet())
+		for ( final Entry< Label > e : inputImg.firstElement().entrySet() )
 			eachCount += e.getCount();
-		
-		final RandomAccessible<LabelMultisetType> extendedImg = Views.extendValue(inputImg, LabelUtils.getOutOfBounds(eachCount));
-		
+
+		final RandomAccessible< LabelMultisetType > extendedImg = Views.extendValue( inputImg, LabelUtils.getOutOfBounds( eachCount ) );
+
 		VolatileLabelMultisetArray downscaledCell;
-		
+
 		int numCellsDownscaled = 0;
-		
-		final N5Writer writer = new N5FSWriter(outputGroupName);
-		final DatasetAttributes writerAttributes = writer.getDatasetAttributes(outputDatasetName);
-		
-		long[] writeLocation = new long[nDim];
-		
-		for(int d = 0; d < nDim; ) {
-			
-			Arrays.setAll(actualLocation, i -> factor[i] * (targetMin[i] + offset[i]));
 
-			// TODO: figure out what part of this is redundant, if any, and clarify it
-			Arrays.setAll(actualSize, i -> Math.min(
- 					factor[i] * (offset[i] + blocksize[i] > targetMin[i] + targetSize[i] ? (targetMin[i] + targetSize[i] - offset[i]) : blocksize[i]),
- 					factor[i] * (int)Math.ceil( (dimensions[i] - actualLocation[i]) / (double)factor[i] ) ));
+		final N5Writer writer = new N5FSWriter( outputGroupName );
+		final DatasetAttributes writerAttributes = writer.getDatasetAttributes( outputDatasetName );
 
-			downscaledCell = LabelMultisetTypeDownscaler.createDownscaledCell(Views.offsetInterval(extendedImg, actualLocation, actualSize), factor);
-			
-			byte[] bytes = new byte[LabelMultisetTypeDownscaler.getSerializedVolatileLabelMultisetArraySize(downscaledCell)];
-			LabelMultisetTypeDownscaler.serializeVolatileLabelMultisetArray(downscaledCell, bytes);
-			
-			for(int i = 0; i < nDim; i ++) writeLocation[i] = (targetMin[i] + offset[i])/blocksize[i];
-			
-			final ByteArrayDataBlock dataBlock = new ByteArrayDataBlock(blocksize, writeLocation, bytes);
-			writer.writeBlock(outputDatasetName, writerAttributes, dataBlock);
-			
+		final long[] writeLocation = new long[ nDim ];
+
+		for ( int d = 0; d < nDim; )
+		{
+
+			Arrays.setAll( actualLocation, i -> factor[ i ] * ( targetMin[ i ] + offset[ i ] ) );
+
+			// TODO: figure out what part of this is redundant, if any, and
+			// clarify it
+			Arrays.setAll( actualSize, i -> Math.min(
+					factor[ i ] * ( offset[ i ] + blocksize[ i ] > targetMin[ i ] + targetSize[ i ] ? targetMin[ i ] + targetSize[ i ] - offset[ i ] : blocksize[ i ] ),
+					factor[ i ] * ( int ) Math.ceil( ( dimensions[ i ] - actualLocation[ i ] ) / ( double ) factor[ i ] ) ) );
+
+			downscaledCell = LabelMultisetTypeDownscaler.createDownscaledCell( Views.offsetInterval( extendedImg, actualLocation, actualSize ), factor );
+
+			final byte[] bytes = new byte[ LabelMultisetTypeDownscaler.getSerializedVolatileLabelMultisetArraySize( downscaledCell ) ];
+			LabelMultisetTypeDownscaler.serializeVolatileLabelMultisetArray( downscaledCell, bytes );
+
+			for ( int i = 0; i < nDim; i++ )
+				writeLocation[ i ] = ( targetMin[ i ] + offset[ i ] ) / blocksize[ i ];
+
+			final ByteArrayDataBlock dataBlock = new ByteArrayDataBlock( blocksize, writeLocation, bytes );
+			writer.writeBlock( outputDatasetName, writerAttributes, dataBlock );
+
 			numCellsDownscaled++;
-			
-			for( d = 0; d < nDim; d++) {
-				offset[d] += blocksize[d];
-				if(offset[d] < targetSize[d])
+
+			for ( d = 0; d < nDim; d++ )
+			{
+				offset[ d ] += blocksize[ d ];
+				if ( offset[ d ] < targetSize[ d ] )
 					break;
 				else
-					offset[d] = 0;
+					offset[ d ] = 0;
 			}
 		}
-		
+
 		return numCellsDownscaled;
 	}
 }
--- src/main/java/bdv/bigcat/spark/SparkDownsampler.java
+++ src/main/java/bdv/bigcat/spark/SparkDownsampler.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.janelia.saalfeldlab.n5.CompressionType;
+import org.janelia.saalfeldlab.n5.Compression;
 import org.janelia.saalfeldlab.n5.DataType;
 import org.janelia.saalfeldlab.n5.DatasetAttributes;
 import org.janelia.saalfeldlab.n5.N5FSReader;
@@ -17,116 +17,127 @@ import org.janelia.saalfeldlab.n5.N5Writer;
 
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
+import com.google.gson.Gson;
 
 public class SparkDownsampler
 {
-	static public class Parameters {
-		
+	static public class Parameters
+	{
+
 		@Parameter( names = { "--igroupname", "--igroup", "-ig" }, description = "Input group name (N5 group)" )
 		public String inputGroupName = null;
-		
+
 		@Parameter( names = { "--idatasetname", "--idata", "-id" }, description = "Input dataset name (N5 relative path from group)" )
 		public String inputDatasetName = null;
-		
-		@Parameter( names = { "--ogroupname", "--ogroup", "-og" }, description = "Output group name (N5 group)")
+
+		@Parameter( names = { "--ogroupname", "--ogroup", "-og" }, description = "Output group name (N5 group)" )
 		public String outputGroupName = null;
-		
-		@Parameter( names = { "--odatasetname", "--odata", "-od" }, description = "Output dataset name (N5 relative path from group)")
+
+		@Parameter( names = { "--odatasetname", "--odata", "-od" }, description = "Output dataset name (N5 relative path from group)" )
 		public String outputDatasetName = null;
-		
-		@Parameter( names = { "--factor", "-f"}, description = "Factor by which to downscale the input image" )
-		public List<Integer> factor = new ArrayList<Integer>();
-		
-		@Parameter( names = { "--parallelblocks", "-pb"}, description = "Size of the blocks (in cells) to parallelize with Spark" )
-		public List<Integer> parallelBlockSize = new ArrayList<Integer>();
-		
-		@Parameter( names = { "--compression", "-c"}, description = "Compression type to use in output N5 dataset" )
+
+		@Parameter( names = { "--factor", "-f" }, description = "Factor by which to downscale the input image" )
+		public List< Integer > factor = new ArrayList<>();
+
+		@Parameter( names = { "--parallelblocks", "-pb" }, description = "Size of the blocks (in cells) to parallelize with Spark" )
+		public List< Integer > parallelBlockSize = new ArrayList<>();
+
+		@Parameter( names = { "--compression", "-c" }, description = "Compression type to use in output N5 dataset" )
 		public String compressionType = "RAW";
-		
-		
-		public boolean init() {
-			if(inputGroupName == null || inputDatasetName == null || factor.size()==0)
+
+		public boolean init()
+		{
+			if ( inputGroupName == null || inputDatasetName == null || factor.size() == 0 )
 				return false;
-			
-			if(parallelBlockSize.size()==0)
-				for(int i = 0; i < factor.size(); i ++)
-					parallelBlockSize.add(16);
-			
-			if(outputGroupName == null)
+
+			if ( parallelBlockSize.size() == 0 )
+				for ( int i = 0; i < factor.size(); i++ )
+					parallelBlockSize.add( 16 );
+
+			if ( outputGroupName == null )
 				outputGroupName = inputGroupName; // default to using same group
-			
-			if(outputDatasetName == null) {
+
+			if ( outputDatasetName == null )
+			{
 				outputDatasetName = inputDatasetName + "-downscaled-";
-				for(int i = 0; i < factor.size(); i ++)
-					outputDatasetName += (i!=0?"x":"") + i;
+				for ( int i = 0; i < factor.size(); i++ )
+					outputDatasetName += ( i != 0 ? "x" : "" ) + i;
 			}
 			return true;
 		}
 	}
-	
-	
-	public static void main(String[] args) throws IOException {
+
+	public static void main( final String[] args ) throws IOException
+	{
 		final Parameters params = new Parameters();
-		JCommander commander = new JCommander( params, args );
-		
-		if(!params.init()) {
+		final JCommander commander = new JCommander( params, args );
+
+		if ( !params.init() )
+		{
 			commander.usage();
 			return;
 		}
-		
-		SparkConf conf = new SparkConf().setAppName( "SparkDownsampler" );
-		JavaSparkContext sc = new JavaSparkContext( conf );
-		SparkDownsampler.downsample(sc,
-				new N5FSReader(params.inputGroupName), params.inputGroupName, params.inputDatasetName,
-				params.factor.stream().mapToInt(i->i).toArray(), params.parallelBlockSize.stream().mapToInt(i->i).toArray(),
+
+		final SparkConf conf = new SparkConf().setAppName( "SparkDownsampler" );
+		final JavaSparkContext sc = new JavaSparkContext( conf );
+		final N5FSReader reader = new N5FSReader( params.inputGroupName );
+		final Gson gson = reader.getGson();
+		SparkDownsampler.downsample( sc,
+				reader,
+				params.inputGroupName,
+				params.inputDatasetName,
+				params.factor.stream().mapToInt( i -> i ).toArray(), params.parallelBlockSize.stream().mapToInt( i -> i ).toArray(),
 				params.outputGroupName, params.outputDatasetName,
-				CompressionType.valueOf(params.compressionType));
+				gson.fromJson( params.compressionType, Compression.class ) );
 	}
 
-	public static void downsample(JavaSparkContext sc,
-			N5Reader reader, String readGroupName, String readDatasetName,
-			int[] downsampleFactor, int[] parallelSize,
-			String outputGroupName, String outputDatasetName,
-			CompressionType compressionType) throws IOException  {
-	
-		DatasetAttributes attributes = reader.getDatasetAttributes(readDatasetName);
+	public static void downsample( final JavaSparkContext sc,
+			final N5Reader reader, final String readGroupName, final String readDatasetName,
+			final int[] downsampleFactor, final int[] parallelSize,
+			final String outputGroupName, final String outputDatasetName,
+			final Compression compression ) throws IOException
+	{
 
-		long[] dimensions = attributes.getDimensions();
-		int[] blockSize = attributes.getBlockSize();
-		
-		List<DownsampleBlock> parallelizeSections = new ArrayList<DownsampleBlock>();
-		
-		int nDim = attributes.getNumDimensions();
-		
-		long[] downsampledDimensions = new long[nDim];
-		Arrays.setAll(downsampledDimensions, i -> (int)(Math.ceil((double)dimensions[i]/downsampleFactor[i])));
+		final DatasetAttributes attributes = reader.getDatasetAttributes( readDatasetName );
 
-		final long[] offset = new long[nDim];
-		int[] actualSize = new int[nDim];
-		
-		for(int d = 0; d < nDim; ) {
-			
-			for(int i = 0; i < nDim; i ++)
-				actualSize[i] = (int) Math.min(parallelSize[i]*blockSize[i], downsampledDimensions[i]-offset[i]);
+		final long[] dimensions = attributes.getDimensions();
+		final int[] blockSize = attributes.getBlockSize();
 
-			parallelizeSections.add(new DownsampleBlock(offset.clone(), actualSize.clone()));
-			
-			for( d = 0; d < nDim; d++) {
-				offset[d] += parallelSize[d]*blockSize[d];
-				if(offset[d] < downsampledDimensions[d])
+		final List< DownsampleBlock > parallelizeSections = new ArrayList<>();
+
+		final int nDim = attributes.getNumDimensions();
+
+		final long[] downsampledDimensions = new long[ nDim ];
+		Arrays.setAll( downsampledDimensions, i -> ( int ) Math.ceil( ( double ) dimensions[ i ] / downsampleFactor[ i ] ) );
+
+		final long[] offset = new long[ nDim ];
+		final int[] actualSize = new int[ nDim ];
+
+		for ( int d = 0; d < nDim; )
+		{
+
+			for ( int i = 0; i < nDim; i++ )
+				actualSize[ i ] = ( int ) Math.min( parallelSize[ i ] * blockSize[ i ], downsampledDimensions[ i ] - offset[ i ] );
+
+			parallelizeSections.add( new DownsampleBlock( offset.clone(), actualSize.clone() ) );
+
+			for ( d = 0; d < nDim; d++ )
+			{
+				offset[ d ] += parallelSize[ d ] * blockSize[ d ];
+				if ( offset[ d ] < downsampledDimensions[ d ] )
 					break;
 				else
-					offset[d] = 0;
+					offset[ d ] = 0;
 			}
 		}
 
-		N5Writer writer = new N5FSWriter(outputGroupName);
-		writer.createDataset(outputDatasetName, downsampledDimensions, blockSize, DataType.UINT8, compressionType);
+		final N5Writer writer = new N5FSWriter( outputGroupName );
+		writer.createDataset( outputDatasetName, downsampledDimensions, blockSize, DataType.UINT8, compression );
 
-		Integer output = sc.parallelize(parallelizeSections)
-			.map( new SparkDownsampleFunction(readGroupName, readDatasetName, downsampleFactor, outputGroupName, outputDatasetName))
-			.reduce( (i,j) -> i+j);
-		
-		System.out.println("Across " + parallelizeSections.size() + " parallelized sections, " + output + " cells were downscaled");
+		final Integer output = sc.parallelize( parallelizeSections )
+				.map( new SparkDownsampleFunction( readGroupName, readDatasetName, downsampleFactor, outputGroupName, outputDatasetName ) )
+				.reduce( ( i, j ) -> i + j );
+
+		System.out.println( "Across " + parallelizeSections.size() + " parallelized sections, " + output + " cells were downscaled" );
 	}
 }
