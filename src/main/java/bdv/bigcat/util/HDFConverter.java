68a223d7e5d0ff2ccc39c0f00c347b7ce66a3ca5
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2018-02-13 16:32:12 -0500

    Remove bigcat dependency
    Depending on a UI/application library is not desirable. It was necessary to extract LabelMultisetType and related classes into separate repositories first.

--- src/main/java/bdv/bigcat/util/HDFConverter.java
+++ src/main/java/bdv/bigcat/util/HDFConverter.java
@@ -6,128 +6,159 @@ import java.util.List;
 
 import org.apache.commons.lang.time.DurationFormatUtils;
 import org.janelia.saalfeldlab.n5.ByteArrayDataBlock;
-import org.janelia.saalfeldlab.n5.CompressionType;
+import org.janelia.saalfeldlab.n5.Compression;
 import org.janelia.saalfeldlab.n5.DataType;
 import org.janelia.saalfeldlab.n5.DatasetAttributes;
-import org.janelia.saalfeldlab.n5.N5;
+import org.janelia.saalfeldlab.n5.GzipCompression;
+import org.janelia.saalfeldlab.n5.N5FSReader;
+import org.janelia.saalfeldlab.n5.N5FSWriter;
 import org.janelia.saalfeldlab.n5.N5Writer;
+import org.janelia.saalfeldlab.n5.hdf5.N5HDF5Reader;
+import org.janelia.saalfeldlab.n5.imglib2.N5Utils;
 
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
+import com.google.gson.Gson;
 
-import bdv.img.cache.VolatileGlobalCellCache;
-import bdv.img.h5.H5LabelMultisetSetupImageLoader;
-import bdv.labels.labelset.LabelMultisetType;
-import bdv.labels.labelset.LabelUtils;
-import ch.systemsx.cisd.hdf5.HDF5Factory;
-import ch.systemsx.cisd.hdf5.IHDF5Reader;
+import net.imglib.type.label.FromIntegerTypeConverter;
+import net.imglib.type.label.LabelMultisetType;
+import net.imglib.type.label.LabelUtils;
 import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.converter.Converters;
+import net.imglib2.type.NativeType;
+import net.imglib2.type.numeric.IntegerType;
 import net.imglib2.util.Intervals;
 import net.imglib2.view.IntervalView;
 import net.imglib2.view.Views;
 
-public class HDFConverter {
+public class HDFConverter
+{
 
+	// TODO make this parallizable/spark and not hdf-to-n5 but convert between
+	// various instances of n5 instead
 	static public class Parameters
 	{
-		@Parameter( names = { "--inputfile", "--input", "-i" }, description = "Input HDF5 file")
+		@Parameter( names = { "--inputfile", "--input", "-i" }, description = "Input HDF5 file" )
 		public String inputHDF5 = null;
-		
-		@Parameter( names = { "--groupname", "--group", "-g" }, description = "Output group name (N5 group)")
+
+		@Parameter( names = { "--groupname", "--group", "-g" }, description = "Output group name (N5 group)" )
 		public String outputGroupName = null;
-		
-		@Parameter( names = { "--datasetname", "--data", "-d" }, description = "Output dataset name (N5 relative path from group)")
+
+		@Parameter( names = { "--datasetname", "--data", "-d" }, description = "Output dataset name (N5 relative path from group)" )
 		public String outputDatasetName = null;
-		
-		@Parameter( names = { "--cellsize", "-cs"}, description = "Size of cells to use in the output N5 dataset" )
-		public List<Integer> cellSize = Arrays.asList(new Integer[] {64, 64, 8});
+
+		@Parameter( names = { "--cellsize", "-cs" }, description = "Size of cells to use in the output N5 dataset" )
+		public List< Integer > cellSize = Arrays.asList( new Integer[] { 64, 64, 8 } );
 
 		@Parameter( names = { "--label", "-l" }, description = "Path to labels in HDF5 file" )
-		public String labelPath = "/volumes/labels/neuron_ids";	
-		
-		@Parameter( names = { "--compression", "-c"}, description = "Compression type to use in output N5 dataset" )
-		public String compressionType = "RAW";	
-		
-		
-		public boolean init() {
-			if(outputGroupName == null || inputHDF5 == null)
+		public String labelPath = "/volumes/labels/neuron_ids";
+
+		@Parameter( names = { "--compression", "-c" }, description = "Compression type to use in output N5 dataset" )
+		public String compressionType = "{\"type\":\"raw\"}";
+
+		public boolean init()
+		{
+			if ( outputGroupName == null || inputHDF5 == null )
 				return false;
-			if(outputDatasetName == null)
-				outputDatasetName = (inputHDF5.contains(".hdf")?inputHDF5.substring(0, inputHDF5.lastIndexOf(".hdf")):inputHDF5);
+			if ( outputDatasetName == null )
+				outputDatasetName = inputHDF5.contains( ".hdf" ) ? inputHDF5.substring( 0, inputHDF5.lastIndexOf( ".hdf" ) ) : inputHDF5;
 			return true;
 		}
 	}
 
-	public static void main(String[] args) throws IOException
+	public static void main( final String[] args ) throws IOException
 	{
 		final Parameters params = new Parameters();
-		JCommander jcomm = new JCommander(params, args);
-		
-		if(!params.init()) {
+		final JCommander jcomm = new JCommander( params, args );
+
+		if ( !params.init() )
+		{
 			jcomm.usage();
 			return;
 		}
-		
-		long startTime = System.currentTimeMillis();
-		
-		int[] cellSize = new int[params.cellSize.size()];
-		Arrays.setAll(cellSize, i -> params.cellSize.get(i));
-		
-		convertHDF5toN5(params.inputHDF5, params.labelPath, cellSize, params.outputGroupName, params.outputDatasetName,
-				CompressionType.valueOf(params.compressionType));
-		
-		long endTime = System.currentTimeMillis();
-		
-		String formattedTime = DurationFormatUtils.formatDuration(endTime-startTime, "HH:mm:ss.SSS");
-		System.out.println("Converted " + params.inputHDF5 + " to N5 dataset at " + params.outputGroupName + " with name " + params.outputDatasetName +
-				" in " + formattedTime);
+
+		final long startTime = System.currentTimeMillis();
+
+		final int[] cellSize = new int[ params.cellSize.size() ];
+		Arrays.setAll( cellSize, i -> params.cellSize.get( i ) );
+
+		final N5FSReader reader = new N5FSReader( params.outputGroupName );
+		final Gson gson = reader.getGson();
+		new N5FSWriter( "/home/phil/local/tmp/123" ).createDataset(
+				"456",
+				new long[] { 1 },
+				new int[] { 1 },
+				DataType.UINT8,
+				new GzipCompression() );
+		final Compression compression = gson.fromJson( params.compressionType, Compression.class );
+
+		convertHDF5toN5(
+				params.inputHDF5,
+				params.labelPath,
+				cellSize,
+				params.outputGroupName,
+				params.outputDatasetName,
+				compression );
+
+		final long endTime = System.currentTimeMillis();
+
+		final String formattedTime = DurationFormatUtils.formatDuration( endTime - startTime, "HH:mm:ss.SSS" );
+		System.out.println( "Converted " + params.inputHDF5 + " to N5 dataset at " + params.outputGroupName + " with name " + params.outputDatasetName +
+				" in " + formattedTime );
 	}
-	
-	public static void convertHDF5toN5(String hdf5Path, String labelsPath,int[] cellDimensions,
-			String outputGroupName, String outputDatasetName, CompressionType compressionType) throws IOException
+
+	public static < I extends IntegerType< I > & NativeType< I > > void convertHDF5toN5(
+			final String hdf5Path,
+			final String labelsPath,
+			final int[] cellDimensions,
+			final String outputGroupName,
+			final String outputDatasetName,
+			final Compression compression ) throws IOException
 	{
-		IHDF5Reader reader = HDF5Factory.open(hdf5Path);
-		
-		VolatileGlobalCellCache cache = new VolatileGlobalCellCache(1, 6);
-		H5LabelMultisetSetupImageLoader loader = new H5LabelMultisetSetupImageLoader(reader, null, labelsPath, 0, cellDimensions , cache);
-		
-		RandomAccessibleInterval<LabelMultisetType> img = loader.getImage(0);
-		
-		int nDim = img.numDimensions();
-		
-		long[] dimensions = new long[nDim];
-		img.dimensions(dimensions);
-		
-		N5Writer n5 = N5.openFSWriter(outputGroupName);
-		n5.createDataset(outputDatasetName, dimensions, cellDimensions, DataType.UINT8, compressionType);
-		
-		final long[] offset = new long[nDim];
-		
-		int[] actualCellDimensions = new int[nDim];
-		
-		for(int d = 0; d < nDim; ) {
-			
-			Arrays.setAll(actualCellDimensions, i -> (int) Math.min(cellDimensions[i], dimensions[i]-offset[i]));
-			
-			IntervalView<LabelMultisetType> tm = Views.offsetInterval(img, offset, Arrays.stream(actualCellDimensions).asLongStream().toArray());
-						
-			final DatasetAttributes attributes = n5.getDatasetAttributes(outputDatasetName);
-			
-			long[] cellOffset = new long[nDim];
-			for(int i = 0; i < nDim; i ++) 
-				cellOffset[i] = offset[i]/cellDimensions[i];
-			
-			final ByteArrayDataBlock dataBlock = new ByteArrayDataBlock(actualCellDimensions, cellOffset,
-					LabelUtils.serializeLabelMultisetTypes(Views.flatIterable(tm), (int)Intervals.numElements(actualCellDimensions)));
-			
-			n5.writeBlock(outputDatasetName, attributes, dataBlock);
-	
-			for( d = 0; d < nDim; d++) {
-				offset[d] += cellDimensions[d];
-				if(offset[d] < dimensions[d])
+		final N5HDF5Reader hdfReader = new N5HDF5Reader( hdf5Path, cellDimensions );
+		final RandomAccessibleInterval< I > img = N5Utils.open( hdfReader, labelsPath );
+
+		final int nDim = img.numDimensions();
+
+		final long[] dimensions = new long[ nDim ];
+		img.dimensions( dimensions );
+
+		final N5Writer n5 = new N5FSWriter( outputGroupName );
+		n5.createDataset( outputDatasetName, dimensions, cellDimensions, DataType.UINT8, compression );
+
+		final long[] offset = new long[ nDim ];
+
+		final int[] actualCellDimensions = new int[ nDim ];
+
+		for ( int d = 0; d < nDim; )
+		{
+
+			Arrays.setAll( actualCellDimensions, i -> ( int ) Math.min( cellDimensions[ i ], dimensions[ i ] - offset[ i ] ) );
+
+			final RandomAccessibleInterval< LabelMultisetType > labelsetImg = Converters.convert(
+					img,
+					new FromIntegerTypeConverter<>(),
+					FromIntegerTypeConverter.geAppropriateType() );
+			final IntervalView< LabelMultisetType > tm = Views.offsetInterval( labelsetImg, offset, Arrays.stream( actualCellDimensions ).asLongStream().toArray() );
+
+			final DatasetAttributes attributes = n5.getDatasetAttributes( outputDatasetName );
+
+			final long[] cellOffset = new long[ nDim ];
+			for ( int i = 0; i < nDim; i++ )
+				cellOffset[ i ] = offset[ i ] / cellDimensions[ i ];
+
+			final ByteArrayDataBlock dataBlock = new ByteArrayDataBlock( actualCellDimensions, cellOffset,
+					LabelUtils.serializeLabelMultisetTypes( Views.flatIterable( tm ), ( int ) Intervals.numElements( actualCellDimensions ) ) );
+
+			n5.writeBlock( outputDatasetName, attributes, dataBlock );
+
+			for ( d = 0; d < nDim; d++ )
+			{
+				offset[ d ] += cellDimensions[ d ];
+				if ( offset[ d ] < dimensions[ d ] )
 					break;
 				else
-					offset[d] = 0;
+					offset[ d ] = 0;
 			}
 		}
 	}
