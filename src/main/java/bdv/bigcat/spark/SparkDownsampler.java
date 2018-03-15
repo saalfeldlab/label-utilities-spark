0e8ce985afc31015076a63e7b2aa26688c109f01
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2018-03-15 14:52:12 -0400

    Expose limit for number of entries as user parameter

--- src/main/java/bdv/bigcat/spark/SparkDownsampler.java
+++ src/main/java/bdv/bigcat/spark/SparkDownsampler.java
@@ -31,6 +31,7 @@ import org.slf4j.LoggerFactory;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import gnu.trove.list.array.TIntArrayList;
 import picocli.CommandLine;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
@@ -56,6 +57,13 @@ public class SparkDownsampler
 
 		@Option( names = { "--block-size", "-b" }, description = "Size of the blocks (in cells) to parallelize with Spark. Format either bx,by,bz or b" )
 		private String[] blockSize;
+
+		@Option(
+				names = { "--max-num-entries", "-m" },
+				required = false,
+				split = ",",
+				description = "Maximum number of multiset entries at each pixels. Values smaller than 1 do not limit the number of entries. Defaults to previous level if not specified. Defaults to -1 for the first level if not specified" )
+		private int[] maxNumEntries;
 
 		@Option( names = { "--compression", "-c" }, description = "Compression type to use in output N5 dataset" )
 		public String compressionType = "{\"type\":\"raw\"}";
@@ -84,6 +92,7 @@ public class SparkDownsampler
 		{
 			LOG.warn( "Will downsample with these factors: {}", Arrays.toString( factors ) );
 			blockSize = blockSize == null ? new String[ 0 ] : blockSize;
+			maxNumEntries = maxNumEntries == null ? new int[] { -1 } : maxNumEntries;
 			final String defaultBlockSize = Arrays.stream( blockSize ).limit( factors.length ).reduce( ( f, s ) -> s ).orElse( "64" );
 			blockSize = Stream.concat(
 					Arrays.stream( blockSize ).limit( factors.length ),
@@ -94,6 +103,7 @@ public class SparkDownsampler
 			final Compression compression = fromString( compressionType );
 
 			final SparkConf conf = new SparkConf().setAppName( "SparkDownsampler" );
+			final TIntArrayList maxNumEntriesList = new TIntArrayList();
 			try (final JavaSparkContext sc = new JavaSparkContext( conf ))
 			{
 
@@ -111,10 +121,15 @@ public class SparkDownsampler
 						LOG.warn( "Dataset for mipmap level already exists at {}, will not create symlink!", linkLocation );
 					}
 				}
+				int lastMaxNumEntries = -1;
 				for ( int factorIndex = 0, level = 1; factorIndex < factors.length; ++factorIndex, ++level )
 				{
 					final String previousScaleLevel = dataset + "/s" + ( level - 1 );
 					final String currentScaleLevel = dataset + "/s" + level;
+
+					final int maxNumEntries = factorIndex < this.maxNumEntries.length ? this.maxNumEntries[ factorIndex ] : lastMaxNumEntries;
+					lastMaxNumEntries = maxNumEntries;
+					maxNumEntriesList.add( maxNumEntries );
 
 					SparkDownsampler.downsample( sc,
 							new N5FSReader( group ),
@@ -124,10 +139,12 @@ public class SparkDownsampler
 							blockSizes[ factorIndex ],
 							group,
 							currentScaleLevel,
-							compression );
+							compression,
+							maxNumEntries );
 				}
 			}
 
+			System.out.println( "YO " + maxNumEntriesList );
 			return null;
 		}
 
@@ -146,7 +163,8 @@ public class SparkDownsampler
 			final int[] blockSize,
 			final String outputGroupName,
 			final String outputDatasetName,
-			final Compression compression ) throws IOException
+			final Compression compression,
+			final int maxNumEntries ) throws IOException
 	{
 
 		final DatasetAttributes attributes = reader.getDatasetAttributes( readDatasetName );
@@ -189,7 +207,7 @@ public class SparkDownsampler
 
 		sc.parallelize( positions )
 				.map( new MinToInterval( max, blockSize ) )
-				.foreach( new SparkDownsampleFunction( readGroupName, readDatasetName, downsampleFactor, outputGroupName, outputDatasetName, 0 ) );
+				.foreach( new SparkDownsampleFunction( readGroupName, readDatasetName, downsampleFactor, outputGroupName, outputDatasetName, maxNumEntries ) );
 
 //		System.out.println( "Across " + positions.size() + " parallelized sections, " + output + " cells were downscaled" );
 	}
