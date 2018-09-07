7fc083bcb4a9cc921897f385c3fd8fe1e3ece3f4
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2018-09-07 08:33:21 -0400

    temporarily curl travis-build.sh from my fork
    https://gitter.im/scijava/scijava-common?at=5b926e30ac25fd11b5f43635

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/master/travis-build.sh
+# curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/master/travis-build.sh
+curl -fsLO https://raw.githubusercontent.com/hanslovsky/scijava-scripts/exclude-saalfeld-maven-repo-from-mirror/travis-build.sh
 sh travis-build.sh $encrypted_99ed6d16f681_key $encrypted_99ed6d16f681_iv
