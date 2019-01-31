saalfeldlab/label-utilities-spark

commit 4a17365eb698e2ee1d8731197cdeb9610c881f5a

Do not mask values that are below threshold in relief

src/main/java/org/janelia/saalfeldlab/label/spark/SparkWatersheds.java
@@ -884,10 +884,7 @@ public class SparkWatersheds {
 			final ArrayImg<FloatType, FloatArray> affinityCrop = ArrayImgs.floats(Intervals.dimensionsAsLongArray(withHalo));
 			final Cursor<FloatType> source = Views.flatIterable(Views.interval(Views.extendValue(affs, new FloatType(Float.NaN)), withHalo)).cursor();
 			final Cursor<FloatType> target = Views.flatIterable(affinityCrop).cursor();
-			while (target.hasNext()) {
-				final double val = source.next().getRealDouble();
-				target.next().setReal(val > minmimumAffinity ? val : Double.NaN);
-			}
+
 			return new Tuple2<>(interval, affinityCrop);
 		}
 	}

commit 81459579c1c9a2a99cb9e679a93b3e1b4a80a39b

Populate affinity crop
Bug introduced in 4a17365eb698e2ee1d8731197cdeb9610c881f5a
Instead of not masking out affinity values below threshold, loop was omitted entirely.

src/main/java/org/janelia/saalfeldlab/label/spark/SparkWatersheds.java
@@ -887,6 +887,7 @@ public class SparkWatersheds {
 			final ArrayImg<FloatType, FloatArray> affinityCrop = ArrayImgs.floats(Intervals.dimensionsAsLongArray(withHalo));
 			final Cursor<FloatType> source = Views.flatIterable(Views.interval(Views.extendValue(affs, new FloatType(Float.NaN)), withHalo)).cursor();
 			final Cursor<FloatType> target = Views.flatIterable(affinityCrop).cursor();
+			while (source.hasNext()) target.next().set(source.next());
 
 			return new Tuple2<>(interval, affinityCrop);
 		}